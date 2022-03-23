ad692fc67637e37d58588ff38043e8873fe1d1d4
Fillipe Marchi <fillipemarchi@gmail.com>
2022-03-23 07:06:34 -0300

    Update gerador_de_medias.js

--- gerador_de_medias.js
+++ gerador_de_medias.js
@@ -1,11 +1,8 @@
 console.log ('SEJA BEM VINDO(A) AO GERADOR DE MÉDIAS!')
 let notas = []
-        alert()
 let nummed = Number(prompt("Insira a quantidade de valores que deseja ter a média: "))
-        alert ()
 for (let cont = 1; cont<=nummed; cont++) 
-  notas[cont] = Number(prompt("Insira o valor "+cont+": ")) 
+notas[cont] = Number(prompt("Insira o valor "+cont+": ")) 
 let soma = notas.reduce((soma, numero)=> soma + numero,0)
 let media = soma / nummed
-        alert ()
 console.log ('Sua média foi de: '+ media.toFixed(1))
